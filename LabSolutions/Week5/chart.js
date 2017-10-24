49e6b014b08bb0896175d561b1ec9cbde8718680
Volodymyr Miz <volodymyr.miz@epfl.ch>
2017-10-24 10:15:36 +0200

    D3 V4 brush

--- LabSolutions/Week5/chart.js
+++ LabSolutions/Week5/chart.js
@@ -14,7 +14,7 @@ const margin = {
   width = chartDiv.clientWidth * 0.95,
   height = chartDiv.clientHeight * 0.5,
   contextHeight = 50;
-contextWidth = width * .5;
+contextWidth = width;
 
 const svg = d3.select("#chart-container").append("svg")
   .attr("width", width + margin.left + margin.right)
@@ -79,34 +79,31 @@ function createChart(data) {
 
   }
 
-// Create Context brush for zooming and scaling 
-  let contextXScale = d3.time.scale()
+  /* Let's create the context brush that will 
+      let us zoom and pan the chart */
+  var contextXScale = d3.scaleTime()
     .range([0, contextWidth])
     .domain(charts[0].xScale.domain());
 
-  let contextAxis = d3.svg.axis()
-    .scale(contextXScale)
+  var contextAxis = d3.axisBottom(contextXScale)
     .tickSize(contextHeight)
-    .tickPadding(-10)
-    .orient("bottom");
+    .tickPadding(-10);
 
-  let contextArea = d3.svg.area()
-    .interpolate("monotone")
+  var contextArea = d3.area()
     .x(function(d) {
       return contextXScale(d.date);
     })
     .y0(contextHeight)
-    .y1(0);
+    .y1(0)
+    .curve(d3.curveLinear);
 
-
- // Create brush 
-  let brush = d3.svg.brush()
-    .x(contextXScale)
+  var brush = d3.brushX()
+    .extent([[contextXScale.range()[0], 0],[contextXScale.range()[1], contextHeight]])
     .on("brush", onBrush);
 
   let context = svg.append("g")
     .attr("class", "context")
-    .attr("transform", "translate(" + (margin.left + width * .25) + "," + (height + margin.top + chartHeight - 10) + ")");
+    .attr("transform", "translate(" + (margin.left) + "," + (height + margin.top + chartHeight - 10) + ")");
 
   context.append("g")
     .attr("class", "x axis top")
@@ -127,8 +124,9 @@ function createChart(data) {
 
   // Brush handler. Get time-range from a brush and pass it to the charts. 
   function onBrush() {
-    let b = brush.empty() ? contextXScale.domain() : brush.extent();
-    for (let i = 0; i < countriesCount; i++) {
+    /* this will return a date range to pass into the chart object */
+    var b = d3.event.selection === null ? contextXScale.domain() : d3.event.selection.map(contextXScale.invert);
+    for (var i = 0; i < countriesCount; i++) {
       charts[i].showOnly(b);
     }
   }
@@ -149,36 +147,47 @@ class Chart{
 
   let localName = this.name;
 
-    // Associate xScale with time 
-  this.xScale = d3.time.scale()
+  /* XScale is time based */
+  this.xScale = d3.scaleTime()
     .range([0, this.width])
     .domain(d3.extent(this.chartData.map(function(d) {
       return d.Year;
     })));
 
-   // Bound yScale using minDataPoint and maxDataPoint 
-  this.yScale = d3.scale.linear()
+  /* YScale is linear based on the maxData Point we found earlier */
+  this.yScale = d3.scaleLinear()
     .range([this.height, 0])
     .domain([this.minDataPoint, this.maxDataPoint]);
   let xS = this.xScale;
   let yS = this.yScale;
 
-  /*  
-      Create the chart. 
-      Here we use 'monotone' interpolation. 
-      Play with the other ones: 'basis', 'linear', 'step-before'. 
-      */ 
-  this.area = d3.svg.area()
-    .interpolate("monotone")
+  /* 
+    This is what creates the chart.
+    There are a number of interpolation options. 
+    'basis' smooths it the most, however, when working with a lot of data, this will slow it down 
+  */
+  this.area = d3.area()
     .x(function(d) {
       return xS(d.Year);
     })
     .y0(this.height)
     .y1(function(d) {
       return yS(d[localName]);
-    });
-
-  // Add the chart to the HTML page 
+    })
+    .curve(d3.curveLinear);
+  /*
+    This isn't required - it simply creates a mask. If this wasn't here,
+    when we zoom/panned, we'd see the chart go off to the left under the y-axis 
+  */
+  this.svg.append("defs").append("clipPath")
+    .attr("id", "clip-" + this.id)
+    .append("rect")
+    .attr("width", this.width)
+    .attr("height", this.height);
+  /*
+    Assign it a class so we can assign a fill color
+    And position it on the page
+  */
   this.chartContainer = svg.append("g")
     .attr('class', this.name.toLowerCase())
     .attr("transform", "translate(" + this.margin.left + "," + (this.margin.top + (this.height * this.id) + (10 * this.id)) + ")");
@@ -189,9 +198,9 @@ class Chart{
     .attr("clip-path", "url(#clip-" + this.id + ")")
     .attr("d", this.area);
 
-  this.xAxisTop = d3.svg.axis().scale(this.xScale).orient("bottom");
-  this.xAxisBottom = d3.svg.axis().scale(this.xScale).orient("top");
-   // show only the top axis 
+  this.xAxisTop = d3.axisBottom(this.xScale);
+  this.xAxisBottom = d3.axisTop(this.xScale);
+  /* We only want a top axis if it's the first country */
   if (this.id == 0) {
     this.chartContainer.append("g")
       .attr("class", "x axis top")
@@ -207,7 +216,7 @@ class Chart{
       .call(this.xAxisBottom);
   }
 
-  this.yAxis = d3.svg.axis().scale(this.yScale).orient("left").ticks(5);
+  this.yAxis = d3.axisLeft(this.yScale).ticks(5);
 
   this.chartContainer.append("g")
     .attr("class", "y axis")
